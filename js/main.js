125d2f2003915f136f33392a8f6eae5b6450487d
kirtimd <mdkirti@gmail.com>
2019-04-14 23:30:26 +0530

    re-added initMap

--- js/main.js
+++ js/main.js
@@ -72,15 +72,15 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  * Initialize Google map, called from HTML.
  */
 window.initMap = () => {
-  // let loc = {
-  //   lat: 40.722216,
-  //   lng: -73.987501
-  // };
-  // self.map = new google.maps.Map(document.getElementById('map'), {
-  //   zoom: 12,
-  //   center: loc,
-  //   scrollwheel: false
-  // });
+  let loc = {
+    lat: 40.722216,
+    lng: -73.987501
+  };
+  self.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: loc,
+    scrollwheel: false
+  });
   updateRestaurants();
 }
 
